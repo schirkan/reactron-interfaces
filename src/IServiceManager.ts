fa3117caaf7eaccdc375bf56c8b72e0fdc48a5e9
Martin Pietschmann <schirkan86@gmx.de>
2018-11-19 13:39:45 +0100

    fix getOptions

--- src/IServiceManager.ts
+++ src/IServiceManager.ts
@@ -4,6 +4,7 @@ import { ICommandResult } from "./ICommandResult";
 export interface IServiceManager {
     getAsync(moduleName: string, serviceName: string): Promise<IReactronService | undefined>;
     get(moduleName: string, serviceName: string): IReactronService | undefined;
+    getOptions(moduleName: string, serviceName: string): any;
     setOptions(moduleName: string, serviceName: string, options: any): Promise<void>;
     startAllServices(): Promise<ICommandResult>;
     stopAllServices(): Promise<ICommandResult>;
