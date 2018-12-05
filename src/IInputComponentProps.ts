5d2dda24165a7044c2ec6a932e8288b9ec41fd40
Martin Pietschmann <schirkan@googlemail.com>
2018-12-05 19:45:31 +0100

    uniqueId in IInputComponentProps

--- src/IInputComponentProps.ts
+++ src/IInputComponentProps.ts
@@ -2,6 +2,7 @@ import { IFieldDefinition } from "./IObjectDefinition";
 
 export interface IInputComponentProps {
     definition: IFieldDefinition;
+    uniqueId: string;
     value: any;
     valueChange: (definition: IFieldDefinition, newValue: any) => void;
 }
