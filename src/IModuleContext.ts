bce6afc8ec11dc5705916ce5ff1bec261b5ec6ac
Martin Pietschmann <schirkan@googlemail.com>
2018-12-11 18:44:55 +0100

    settings als prop

--- src/IModuleContext.ts
+++ src/IModuleContext.ts
@@ -5,7 +5,7 @@ export interface IModuleContext {
     readonly backendService: IBackendService;
     readonly moduleStorage: ElectronStore;
     readonly moduleName: string;
+    readonly settings: Readonly<ISystemSettings>;
 
-    getService<TService = any>(serviceName: string, moduleName?: string): TService | undefined;
-    getSettings(): Readonly<ISystemSettings>;
+    getService<TService = any>(serviceName: string, moduleName?: string): TService | undefined;    
 }
