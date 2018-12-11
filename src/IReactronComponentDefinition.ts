f607814f0874ef288da92e58dafc4f701ed6a636
Martin Pietschmann <schirkan@googlemail.com>
2018-12-11 20:12:31 +0100

    new component type 'internal'

--- src/IReactronComponentDefinition.ts
+++ src/IReactronComponentDefinition.ts
@@ -1,6 +1,6 @@
 import { IObjectDefinition } from "./IObjectDefinition";
 
-type ComponentType = 'content' | 'admin-input' | 'layout' | 'frame';
+type ComponentType = 'content' | 'admin-input' | 'layout' | 'frame' | 'internal';
 
 export interface IReactronComponentDefinition<TOptions = any> extends IObjectDefinition {
     component: new (props: TOptions) => React.Component<TOptions>;
