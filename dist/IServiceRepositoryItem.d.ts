f4005900c73f079bff201c4f1335390c673247f1
Martin Pietschmann <Martin.Pietschmann@itergo.com>
2019-01-15 17:34:00 +0100

    context in IServiceRepositoryItem

--- dist/IServiceRepositoryItem.d.ts
+++ dist/IServiceRepositoryItem.d.ts
@@ -1,8 +1,10 @@
 import { IReactronService } from "./IReactronService";
 import { IReactronServiceDefinition } from "./IReactronServiceDefinition";
+import { IReactronServiceContext } from "./IReactronServiceContext";
 export interface IServiceRepositoryItem extends IReactronServiceDefinition {
     moduleName: string;
     instance: IReactronService;
+    context: IReactronServiceContext;
     state: 'starting' | 'running' | 'stopped' | 'error';
     log: string[];
 }
--- src/IServiceRepositoryItem.ts
+++ src/IServiceRepositoryItem.ts
@@ -1,9 +1,11 @@
 import { IReactronService } from "./IReactronService";
 import { IReactronServiceDefinition } from "./IReactronServiceDefinition";
+import { IReactronServiceContext } from "./IReactronServiceContext";
 
 export interface IServiceRepositoryItem extends IReactronServiceDefinition {
     moduleName: string;
     instance: IReactronService;
+    context: IReactronServiceContext;
     state: 'starting' | 'running' | 'stopped' | 'error';
     log: string[];
 }
