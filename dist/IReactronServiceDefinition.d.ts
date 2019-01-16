1a4c2bdc65537a9f3fdc940f9eab723120360c4c
Martin Pietschmann <Martin.Pietschmann@itergo.com>
2019-01-16 09:20:55 +0100

    build

--- dist/IReactronServiceDefinition.d.ts
+++ dist/IReactronServiceDefinition.d.ts
@@ -1,5 +1,6 @@
 import { IObjectDefinition } from "./IObjectDefinition";
 import { IReactronService } from "./IReactronService";
+import { IReactronServiceContext } from "./IReactronServiceContext";
 export interface IReactronServiceDefinition<TOptions = any> extends IObjectDefinition {
-    service: new () => IReactronService<TOptions>;
+    service: new (context: IReactronServiceContext) => IReactronService<TOptions>;
 }
