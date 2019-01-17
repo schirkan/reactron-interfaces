70e5acf9b766ea588c7a6ff9dc05ba26f79ac8ba
Martin Pietschmann <Martin.Pietschmann@itergo.com>
2019-01-17 09:38:53 +0100

    refac IPubSub

--- src/IPubSub.ts
+++ src/IPubSub.ts
@@ -1,13 +1,13 @@
-export interface IPubSubEvgent {
+export interface IPubSubEvent {
     name: string;
-    resolve: (data: any) => void;
-    reject: (reason: any) => void;
 }
 
+export type PubSubListener = (event: IPubSubEvent, ...args: any[]) => void;
+
 export interface IPubSub {
-    subscribe(eventName: string, listener: (event: IPubSubEvgent, ...args: any[]) => void): void;
-    once(eventName: string, listener: (event: IPubSubEvgent, ...args: any[]) => void): void;
-    unsubscribe(eventName: string, listener: (event: IPubSubEvgent, ...args: any[]) => void): void;
-    publish(eventName: string, ...args: any[]): Promise<any>;
+    subscribe(eventName: string, listener: PubSubListener): void;
+    once(eventName: string, listener: PubSubListener): void;
+    unsubscribe(eventName: string, listener: PubSubListener): void;
+    publish(eventName: string, ...args: any[]): void;
     clearAllSubscriptions(): void;
 }
