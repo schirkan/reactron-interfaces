3fb30b90fddddb438e11e7bba41f53578a04d1c4
Martin Pietschmann <Martin.Pietschmann@itergo.com>
2019-01-24 17:36:20 +0100

    remove backendService from context

--- dist/IModuleContext.d.ts
+++ dist/IModuleContext.d.ts
@@ -1,9 +1,7 @@
-import { IBackendService } from './IBackendService';
 import { ISystemSettings } from './ISystemSettings';
 import { ElectronStore } from './ElectronStore';
 import { IReactronServices } from './IReactronServices';
 export interface IModuleContext {
-    readonly backendService: IBackendService;
     readonly moduleStorage: ElectronStore;
     readonly moduleName: string;
     readonly settings: Readonly<ISystemSettings>;
